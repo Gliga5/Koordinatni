848b0c59f243a653486f227e3846c889c125fb06
Gliga5 <lukagligoric5@gmail.com>
2019-11-03 20:10:36 +0100

    Update!
    Use "Inspect" in your browser! (shortcut: Ctrl+Shift+I)
    
    Functions!
     ks.tacka(x,y) - make a dot on coordinates x and y (x and y are numbers)
     ks.povezi(bool) - connect the dots, true - connect all dots, false - connect all dots excluding last connection (between last and first dot)

--- sketch.js
+++ sketch.js
@@ -7,19 +7,5 @@ function setup(){
 }
 function draw(){
     background(244);
-    ks.nacrtaj(true); 
-    let input = window.prompt("Koliko tacki");
-
-    for(let i = 1; i <= input; i++){
-        let input1 = window.prompt(`Koordinate\nNapisi x${i}`);
-        let input2 = window.prompt(`Koordinate\nNapisi y${i}`);
-
-        ks.tacka(input1,input2);
-    }
-    let input2 = window.prompt("Povezati tacke?\n0 - da, u liniji\n1 - da, sve");
-    if (input2 == "0"){
-        ks.povezi();
-    }else if (input2 == "1"){
-        ks.povezi(true);
-    }
+    ks.nacrtaj(true);
 }
